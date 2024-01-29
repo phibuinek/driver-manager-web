5be577ce39d675799a14bdb0ea387dd8269d352e
duyvu29 <87415259+duyvu29@users.noreply.github.com>
2024-01-29 17:02:42 +0700

    process

--- src/app/components/Home/FeaturesSection.tsx
+++ src/app/components/Home/FeaturesSection.tsx
@@ -6,7 +6,7 @@
 //           <h2 className="text-3xl text-[#252525] font-bold mb-2">LỢI ÍCH CHÍNH CỦA CHÚNG TÔI CAM KẾT CHO KHÁCH HÀNG</h2>
 //           <div className="inline-block w-16 h-1 bg-green-500 mb-6"></div>
 //         </div>
-  
+
 //         <div className="max-w-4xl mx-auto px-6">
 //           <div className="grid grid-cols-2 md:grid-cols-2 gap-16">
 //             {/* Feature 1 */}
@@ -26,8 +26,8 @@
 //                 <li>Số liệu minh bạch cho từng chuyến</li>
 //               </ul>
 //               </div>
-//             </div>  
-            
+//             </div>
+
 //             {/* Feature 2 */}
 //             <div className="bg-[#FFFFFF] p-8 rounded-lg shadow-md">
 //               <h3 className="text-xl text-[#252525] font-semibold mb-4">Quản lý thuận tiện</h3>
@@ -43,73 +43,118 @@
 //       </div>
 //     );
 //   };
-  
+
 //   export default FeaturesSection;
 import Image from "next/image";
 
 const FeaturesSection = () => {
   return (
     <div className=" m-16">
-      <div className="grid grid-cols-4 space-x-5">
-        <div className="col-span-1  mt-40">
-          <div className="my-48 mx-4 border-2 rounded-lg outline-1 shadow-lg shadow-gray-400 outline-gray-200">
-            <div className="text-2xl text-center justify-center items-center rounded-2xl bg-gradient-to-r from-[#21d6b9] to-[#65de76] text-white font-bold mx-4 my-4 py-2">VƯỢT TRỘI - TIN CẬY</div>
-            <div className="block text-5xl mx-6 text-center leading-12 text-primary font-medium my-12">NHỮNG LỢI ÍCH CHÚNG TÔI CAM KẾT VỚI KHÁCH HÀNG</div>
-          </div>
-        </div>
-        <div className="col-span-3 grid grid-rows-3 space-y-0">
-          <div className="row-span-1 mt-40">
-            <div className="mt-4 mx-4 border-2 rounded-lg bg-gray-200 shadow-lg shadow-gray-400">
-             <div className="flex">
-              <div className="text-2xl text-left rounded-2xl bg-gradient-to-r from-[#21d6b9] to-[#65de76] border-2 text-white font-bold mx-4 my-4 py-2 px-4">THỐNG KÊ CHÍNH XÁC</div>
-             </div>
-              <div className="flex">
-                <ul>
-                  <li className="list-disc mx-6 text-left leading-12 text-black font-medium mt-4">Dữ Liệu Tuyệt Đối Chính Xác: Cam kết cung cấp dữ liệu thống kê và hoạt động đội xe
-                  một cách chính xác tuyệt đối, đảm bảo độ tin cậy cho quyết định kinh doanh.</li>
-                  <li className="list-disc mx-6 text-left leading-12 text-black font-medium my-4">Bảng Thống Kê Thông Minh: Sử dụng công nghệ tiên tiến để tạo ra bảng thống kê thông
-                  minh, giúp khách hàng dễ dàng theo dõi hiệu suất và tình hình vận hành mọi lúc, mọi nơi.</li>
-                </ul>
+      <div className="hidden md:block">
+        <div className="grid grid-cols-4 space-x-5">
+          <div className="col-span-1  mt-40">
+            <div className="my-48 mx-4 border-2 rounded-lg outline-1 shadow-lg shadow-gray-400 outline-gray-200">
+              <div className="text-2xl text-center justify-center items-center rounded-2xl bg-gradient-to-r from-[#21d6b9] to-[#65de76] text-white font-bold mx-4 my-4 py-2 ">
+                VƯỢT TRỘI - TIN CẬY
+              </div>
+              <div className="block text-5xl mx-6 text-center leading-12 text-primary font-medium my-12">
+                NHỮNG LỢI ÍCH CHÚNG TÔI CAM KẾT VỚI KHÁCH HÀNG
               </div>
             </div>
           </div>
-          <div className="row-span-1">
-            <div className="border-2 rounded-lg bg-gray-200 shadow-lg shadow-gray-400 mt-14 mx-4">
-             <div className="flex">
-              <div className="text-2xl text-left rounded-2xl bg-gradient-to-r from-[#21d6b9] to-[#65de76] border-2 text-white font-bold mx-4 my-4 py-2 px-4">QUẢN LÝ THUẬN TIỆN</div>
-             </div>
-              <div className="flex">
-                <ul>
-                  <li className="list-disc mx-6 text-left leading-12 text-black font-medium mt-4">Giao Diện Thân Thiện Người Dùng: Thiết kế giao diện đơn giản và thân thiện giúp quản lý
-                  dễ dàng và linh hoạt, kể cả đối với người dùng không chuyên nghiệp về công nghệ.</li>
-                  <li className="list-disc mx-6 text-left leading-12 text-black font-medium my-4">Quản Lý Từ Xa: Cam kết cung cấp khả năng quản lý đội xe từ xa thông qua ứng dụng di
-                  động, giúp doanh nghiệp kiểm soát mọi hoạt động mọi lúc, mọi nơi.</li>
-                  <li className="list-disc mx-6 text-left leading-12 text-black font-medium my-4">Tích Hợp Công Nghệ Mới Nhất: Liên tục cập nhật và tích hợp công nghệ mới nhất để cung
-                  cấp trải nghiệm quản lý thuận tiện và đáp ứng nhanh chóng với những thay đổi trong ngành vận tải.</li>
-                </ul>
+          <div className="col-span-3 grid grid-rows-3 space-y-0">
+            <div className="row-span-1 mt-40">
+              <div className="mt-4 mx-4 border-2 rounded-lg bg-gray-200 shadow-lg shadow-gray-400">
+                <div className="flex">
+                  <div className="text-2xl text-left rounded-2xl bg-gradient-to-r from-[#21d6b9] to-[#65de76] border-2 text-white font-bold mx-4 my-4 py-2 px-4">
+                    THỐNG KÊ CHÍNH XÁC
+                  </div>
+                </div>
+                <div className="flex">
+                  <ul>
+                    <li className="list-disc mx-6 text-left leading-12 text-black font-medium mt-4">
+                      Dữ Liệu Tuyệt Đối Chính Xác: Cam kết cung cấp dữ liệu
+                      thống kê và hoạt động đội xe một cách chính xác tuyệt đối,
+                      đảm bảo độ tin cậy cho quyết định kinh doanh.
+                    </li>
+                    <li className="list-disc mx-6 text-left leading-12 text-black font-medium my-4">
+                      Bảng Thống Kê Thông Minh: Sử dụng công nghệ tiên tiến để
+                      tạo ra bảng thống kê thông minh, giúp khách hàng dễ dàng
+                      theo dõi hiệu suất và tình hình vận hành mọi lúc, mọi nơi.
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="row-span-1">
-            <div className=" mx-4 border-2 rounded-lg bg-gray-200 shadow-lg shadow-gray-400">
-             <div className="flex">
-              <div className="text-2xl text-left rounded-2xl bg-gradient-to-r from-[#21d6b9] to-[#65de76] border-2 text-white font-bold mx-4 my-4 py-2 px-4">HỖ TRỢ TẬN TÂM</div>
-             </div>
-              <div className="flex">
-                <ul>
-                  <li className="list-disc mx-6 text-left leading-12 text-black font-medium mt-4">Đội Ngũ Hỗ Trợ Chuyên Nghiệp: Cam kết đưa ra đội ngũ hỗ trợ chuyên nghiệp, sẵn sàng giải
-                  đáp mọi thắc mắc và hỗ trợ khách hàng trong quá trình sử dụng.</li>
-                  <li className="list-disc mx-6 text-left leading-12 text-black font-medium my-4">Hướng Dẫn Sử Dụng Chi Tiết: Cung cấp hướng dẫn sử dụng chi tiết và tài nguyên học liệu
-                  để giúp người dùng nắm vững chức năng và tận dụng đầy đủ tiềm năng của hệ thống.</li>
-                  <li className="list-disc mx-6 text-left leading-12 text-black font-medium my-4">Liên Tục Cải Tiến: Tích cực lắng nghe phản hồi từ khách hàng và liên tục cải tiến hệ thống để
-                  đảm bảo rằng nó luôn đáp ứng được các nhu cầu và mong muốn mới nhất của người dùng.</li>
-                </ul>
+            <div className="row-span-1">
+              <div className="border-2 rounded-lg bg-gray-200 shadow-lg shadow-gray-400 mt-14 mx-4">
+                <div className="flex">
+                  <div className="text-2xl text-left rounded-2xl bg-gradient-to-r from-[#21d6b9] to-[#65de76] border-2 text-white font-bold mx-4 my-4 py-2 px-4">
+                    QUẢN LÝ THUẬN TIỆN
+                  </div>
+                </div>
+                <div className="flex">
+                  <ul>
+                    <li className="list-disc mx-6 text-left leading-12 text-black font-medium mt-4">
+                      Giao Diện Thân Thiện Người Dùng: Thiết kế giao diện đơn
+                      giản và thân thiện giúp quản lý dễ dàng và linh hoạt, kể
+                      cả đối với người dùng không chuyên nghiệp về công nghệ.
+                    </li>
+                    <li className="list-disc mx-6 text-left leading-12 text-black font-medium my-4">
+                      Quản Lý Từ Xa: Cam kết cung cấp khả năng quản lý đội xe từ
+                      xa thông qua ứng dụng di động, giúp doanh nghiệp kiểm soát
+                      mọi hoạt động mọi lúc, mọi nơi.
+                    </li>
+                    <li className="list-disc mx-6 text-left leading-12 text-black font-medium my-4">
+                      Tích Hợp Công Nghệ Mới Nhất: Liên tục cập nhật và tích hợp
+                      công nghệ mới nhất để cung cấp trải nghiệm quản lý thuận
+                      tiện và đáp ứng nhanh chóng với những thay đổi trong ngành
+                      vận tải.
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="row-span-1">
+              <div className=" mx-4 border-2 rounded-lg bg-gray-200 shadow-lg shadow-gray-400">
+                <div className="flex">
+                  <div className="text-2xl text-left rounded-2xl bg-gradient-to-r from-[#21d6b9] to-[#65de76] border-2 text-white font-bold mx-4 my-4 py-2 px-4">
+                    HỖ TRỢ TẬN TÂM
+                  </div>
+                </div>
+                <div className="flex">
+                  <ul>
+                    <li className="list-disc mx-6 text-left leading-12 text-black font-medium mt-4">
+                      Đội Ngũ Hỗ Trợ Chuyên Nghiệp: Cam kết đưa ra đội ngũ hỗ
+                      trợ chuyên nghiệp, sẵn sàng giải đáp mọi thắc mắc và hỗ
+                      trợ khách hàng trong quá trình sử dụng.
+                    </li>
+                    <li className="list-disc mx-6 text-left leading-12 text-black font-medium my-4">
+                      Hướng Dẫn Sử Dụng Chi Tiết: Cung cấp hướng dẫn sử dụng chi
+                      tiết và tài nguyên học liệu để giúp người dùng nắm vững
+                      chức năng và tận dụng đầy đủ tiềm năng của hệ thống.
+                    </li>
+                    <li className="list-disc mx-6 text-left leading-12 text-black font-medium my-4">
+                      Liên Tục Cải Tiến: Tích cực lắng nghe phản hồi từ khách
+                      hàng và liên tục cải tiến hệ thống để đảm bảo rằng nó luôn
+                      đáp ứng được các nhu cầu và mong muốn mới nhất của người
+                      dùng.
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+      <div className="md:hidden  flex flex-col items-center justify-center">
+        <div>
+          <h1 className="text-center font-bold">
+            NHỮNG LỢI ÍCH CHÚNG TÔI CAM KẾT VỚI KHÁCH HÀNG
+          </h1>
+        </div>
+      </div>
     </div>
-      );
-    };
+  );
+};
 export default FeaturesSection;
--- src/app/components/Home/Header.tsx
+++ src/app/components/Home/Header.tsx
@@ -29,33 +29,121 @@
 
 // export default Header;
 
-
 import Link from "next/link";
 import Image from "next/image";
-
+import { useState } from "react";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   return (
     <header className="py-4">
       <div className="container mx-auto flex items-center justify-between px-4">
-        <div className="block items-center space-x-4">
+        <div className="block items-center space-x-4 ">
           {/* Sử dụng Image component của Next.js nếu bạn có một file ảnh, hoặc thẻ img thông thường */}
-          <Image className="items-center ml-10" src={require("../../../../public/assets/logo/logo-app.png")} alt="" width={70} height={70} />
-          <h1 className="font-bold text-xl text-[#2FA060]">Driver Manager</h1>
-          
+          <Image
+            className="items-center w-20 h-20"
+            src={require("../../../../public/assets/logo/logo-app.png")}
+            alt=""
+            width={70}
+            height={70}
+          />
+          <h1 className="font-bold text-xl text-[#2FA060] hidden md:block">
+            Driver Manager
+          </h1>
         </div>
-        <div className="flex items-center space-x-20 font-semibold">   
-          <button className="active:text-primary hover:text-primary focus:text-primary"><a href="#">Trang chủ</a></button>
-          {/* <button className="active:text-primary hover:text-primary focus:text-primary"><a href="#product">Sản phẩm</a></button> */}
-          <button className="active:text-primary hover:text-primary focus:text-primary">
-          
-          <a href="/Privacy">Điều khoản</a>
+        <div className="flex items-center space-x-20 font-semibold">
+          <button className="active:text-primary hover:text-primary focus:text-primary hidden md:block">
+            <a href="#">Trang chủ</a>
           </button>
-          <button className="active:text-primary hover:text-primary focus:text-primary"><a href="#footer">Liên hệ</a></button>
-        </div>  
-        <Link href="/login" className="mr-10 bg-inherit text-primary font-semibold px-6 py-2 rounded-lg outline text-base hover:bg-green-400 transition-colors hover:text-white">
-            Đăng nhập
+          {/* <button className="active:text-primary hover:text-primary focus:text-primary"><a href="#product">Sản phẩm</a></button> */}
+          <button className="active:text-primary hover:text-primary focus:text-primary hidden md:block">
+            <a href="/Privacy">Điều khoản</a>
+          </button>
+          <button className="active:text-primary hover:text-primary focus:text-primary hidden md:block">
+            <a href="#footer">Liên hệ</a>
+          </button>
+        </div>
+        <Link
+          href="/login"
+          className="mr-10 bg-inherit text-primary font-semibold px-6 py-2 rounded-lg outline text-base hover:bg-green-400 transition-colors hover:text-white hidden md:block"
+        >
+          Đăng nhập
         </Link>
+        {/* Nút để mở sidebar */}
+        <button className="p-2 text-white md:hidden" onClick={toggleSidebar}>
+          <Image
+            className="max-w-none"
+            src={require("/public/assets/logo/menu.svg")}
+            width={25}
+            height={25}
+            alt="Search"
+          ></Image>
+        </button>
+
+        {/* Sidebar */}
+        <div
+          className={`fixed inset-y-0 left-0 transform ${
+            sidebarOpen ? "translate-x-0" : "-translate-x-full"
+          } w-64 bg-green-500 z-50 transition duration-300 ease-in-out`}
+        >
+          {/* Nút đóng sidebar */}
+          <button
+            className="flex items-center justify-end p-2 text-white w-full"
+            onClick={toggleSidebar}
+          >
+            <Image
+              className="max-w-none"
+              src={require("/public/assets/cancel.svg")}
+              width={25}
+              height={25}
+              alt="cancel"
+            />
+          </button>
+          <ul>
+            <li className="border-b border-gray-200">
+              <a
+                href="#"
+                className="block py-2 px-4 text-lg hover:bg-gray-100 text-white"
+              >
+                Trang chủ
+              </a>
+            </li>
+            <li className="border-b border-gray-200">
+              <a
+                href="/#Privacy"
+                className="block py-2 px-4 text-lg hover:bg-gray-100 text-white"
+              >
+                Điều khoản
+              </a>
+            </li>
+            <li className="border-b border-gray-200">
+              <a
+                href="/#footer"
+                className="block py-2 px-4 text-lg hover:bg-gray-100 text-white"
+              >
+                Liên hệ
+              </a>
+            </li>
+            <li className="border-b border-gray-200">
+              <a
+                href="/login"
+                className="block py-2 px-4 text-lg hover:bg-gray-100 text-white"
+              >
+                Đăng nhập
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </header>
   );
